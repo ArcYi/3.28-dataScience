cd3b52005eaaccd0bfbea3791b9f6a3333d06e5c
unknown <bryanchen030317@outlook.com>
2020-04-25 08:41:51 +0800

    lecture 5 start

---
+++ lec5/code/linearRegression.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class LinearRegression:
+    
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        return self.w * X + self.b
